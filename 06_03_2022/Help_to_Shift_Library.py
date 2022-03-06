ef937ffe9b3a42637b181b96e6d10847d69abcda
Param Kansagra <param.j.kansagra@gmail.com>
2022-03-06 20:45:12 +0530

    Logic Change

--- 06_03_2022/Help_to_Shift_Library.py
+++ 06_03_2022/Help_to_Shift_Library.py
@@ -2,35 +2,33 @@ n = int(input())
 ramu = 0
 somu = 0
 trips = 0
-last = ""
-
-while(True):
+last_person = ""
+for i in range(n):
     if(ramu + somu >= n):
         break
+    trips += 1
+    if(trips&1):
+        ramu += trips
+        if(ramu + somu > n):
+            ramu -= (ramu + somu - n)
+            last = "Ramu"
+            break
+        somu += trips * 2
+        if(ramu + somu > n):
+            somu -= (ramu + somu - n)
+            last = "Somu"
+            break
     else:
-        trips += 1
-        if(trips%2 != 0):
-            ramu += trips
-            if(ramu + somu > n):
-                ramu -= (ramu + somu -n)
-                last = "Ramu"
-                break
-            somu += 2*trips
-            if(ramu + somu > n):
-                somu -= (somu + ramu - n)
-                last = "Somu"
-                break
-        else:
-            ramu += 2*trips
-            if(ramu + somu > n):
-                ramu -= (somu + ramu - n)
-                last = "Ramu"
-                break
-            somu += trips
-            if(ramu + somu > n):
-                somu -= (ramu + somu - n)
-                last = "Somu"
-                break
+        ramu += trips * 2
+        if(ramu + somu > n):
+            ramu -= (ramu + somu - n)
+            last = "Ramu"
+            break
+        somu += trips
+        if(ramu + somu > n):
+            somu -= (ramu + somu - n)
+            last = "Somu"
+            break
 print(trips)
 print(ramu,somu,sep="\t")
 print(last)
